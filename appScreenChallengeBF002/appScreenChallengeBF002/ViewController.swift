c79414af715e5dcabd551309ca340538015bf89e
Wesley Prado <wesleypradobjj@gmail.com>
2022-09-26 18:32:28 +0400

    Second Commit / Visual and logic implemented!

--- appScreenChallengeBF002/appScreenChallengeBF002/ViewController.swift
+++ appScreenChallengeBF002/appScreenChallengeBF002/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
-
+    
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var subtitleLabel: UILabel!
     
@@ -17,7 +17,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var adressLabel: UILabel!
     @IBOutlet weak var adressTextField: UITextField!
-   
+    
     @IBOutlet weak var passwordLabel: UILabel!
     @IBOutlet weak var passwordTextField: UITextField!
     
@@ -42,22 +42,22 @@ class ViewController: UIViewController, UITextFieldDelegate {
         titleLabel.textAlignment = .center
         titleLabel.font = .boldSystemFont(ofSize: 30)
         titleLabel.textColor = .white
-
+        
         subtitleLabel.text = "Tela de Cadastro 02"
         subtitleLabel.textAlignment = .center
         subtitleLabel.font = .boldSystemFont(ofSize: 18)
         subtitleLabel.textColor = .black
-
+        
         nameLabel.text = "Nome:"
         nameLabel.textAlignment = .left
         nameLabel.font = .boldSystemFont(ofSize: 15)
         nameLabel.textColor = .black
-
+        
         adressLabel.text = "Endereço:"
         adressLabel.textAlignment = .left
         adressLabel.font = .boldSystemFont(ofSize: 15)
         adressLabel.textColor = .black
-
+        
         passwordLabel.text = "Senha:"
         passwordLabel.textAlignment = .left
         passwordLabel.font = .boldSystemFont(ofSize: 15)
@@ -74,11 +74,11 @@ class ViewController: UIViewController, UITextFieldDelegate {
         nameTextField.placeholder = "Por favor, digite seu nome."
         nameTextField.textColor = UIColor.black
         nameTextField.delegate = self
-
+        
         adressTextField.placeholder = "Por favor, digite seu endereço."
         adressTextField.textColor = UIColor.black
         adressTextField.delegate = self
-
+        
         passwordTextField.placeholder = "Por favor, digite sua senha."
         passwordTextField.textColor = UIColor.black
         passwordTextField.delegate = self
@@ -96,24 +96,24 @@ class ViewController: UIViewController, UITextFieldDelegate {
         registerButton.layer.cornerRadius = 10
         
     }
-
+    
     //Does the text field border color should immediately turn blue  when the keyboard rises?
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
         print("TextFieldDidBeginEditing-ok")
-        //textField.layer.borderColor = UIColor.blue.cgColor
-//        switch textField {
-//        case nameTextField:
-//             nameTextField.layer.borderColor = UIColor.blue.cgColor
-//
-//        case adressTextField:
-//            adressTextField.layer.borderColor = UIColor.blue.cgColor
-//
-//        case passwordTextField:
-//            passwordTextField.layer.borderColor = UIColor.blue.cgColor
-//
-//        default:
-//            break
+        textField.layer.borderColor = UIColor.blue.cgColor
+        switch textField {
+        case nameTextField:
+            nameTextField.layer.borderColor = UIColor.blue.cgColor
+            
+        case adressTextField:
+            adressTextField.layer.borderColor = UIColor.blue.cgColor
+            
+        case passwordTextField:
+            passwordTextField.layer.borderColor = UIColor.blue.cgColor
+            
+        default:
+            break
         }
         
     }
@@ -121,27 +121,32 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     func textFieldDidEndEditing(_ textField: UITextField) {
         print("TextFieldDidEndEditing-ok")
-//        if textField.text == ""{
-//            textField.layer.borderWidth = 2
-//            textField.layer.borderColor = UIColor.red.cgColor
-//        } else {
-//            textField.layer.borderWidth = 0
-//        }
-//
-//        //Validação do botão
-//        if nameTextField.text == "" || adressTextField.text == "" || passwordTextField.text == ""{
-//                   registerButton.isEnabled = false
-//               } else {
-//                   registerButton.isEnabled = true
-//               }
+        if textField.text == ""{
+            textField.layer.borderWidth = 2
+            textField.layer.borderColor = UIColor.red.cgColor
+        } else {
+            textField.layer.borderWidth = 0
+        }
+        
+        //Validação do botão
+        if passwordTextField.text != confirmPasswordTextField.text{
+            registerButton.isEnabled = false
+            passwordTextField.layer.borderWidth = 2
+            passwordTextField.layer.borderColor = UIColor.red.cgColor
+            confirmPasswordTextField.layer.borderWidth = 2
+            confirmPasswordTextField.layer.borderColor = UIColor.red.cgColor
+        } else {
+            registerButton.isEnabled = true
+        }
     }
     
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        print("TextFieldShouldReturn-OK")
+        print("Cadastro realizado com sucesso!")
         textField.resignFirstResponder()
         return true
     }
+}
     
 
 
